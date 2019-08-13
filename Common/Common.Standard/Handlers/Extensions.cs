c3d8daa12a33f829cb25c3fdb49c94fb5d315fb7
Mateusz Bieńkowski <bi3niol@gmail.com>
2019-08-13 22:01:39 +0200

    ext

---
+++ Common/Common.Standard/Handlers/Extensions.cs
@@ -0,0 +1,56 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Common.Standard.Handlers
+{
+    public static class Extensions
+    {
+        public static ContainerBuilder AddHandlers(this ContainerBuilder builder, params Assembly[] assemblies)
+        {
+            if (assemblies == null || assemblies.Length == 0)
+                assemblies = new Assembly[] { Assembly.GetCallingAssembly() };
+
+            builder.AddQueryHandlers(assemblies);
+            builder.AddCommandHandlers(assemblies);
+            builder.AddEventHandlers(assemblies);
+
+            return builder;
+        }
+        public static ContainerBuilder AddQueryHandlers(this ContainerBuilder builder, params Assembly[] assemblies)
+        {
+            if (assemblies == null || assemblies.Length == 0)
+                assemblies = new Assembly[] { Assembly.GetCallingAssembly() };
+
+            builder.RegisterAssemblyTypes(assemblies)
+             .AsClosedTypesOf(typeof(IQueryHandler<,>))
+             .InstancePerDependency();
+
+            return builder;
+        }
+        public static ContainerBuilder AddCommandHandlers(this ContainerBuilder builder, params Assembly[] assemblies)
+        {
+            if (assemblies == null || assemblies.Length == 0)
+                assemblies = new Assembly[] { Assembly.GetCallingAssembly() };
+
+            builder.RegisterAssemblyTypes(assemblies)
+             .AsClosedTypesOf(typeof(ICommandHandler<>))
+             .InstancePerDependency();
+
+            return builder;
+        }
+        public static ContainerBuilder AddEventHandlers(this ContainerBuilder builder, params Assembly[] assemblies)
+        {
+            if (assemblies == null || assemblies.Length == 0)
+                assemblies = new Assembly[] { Assembly.GetCallingAssembly() };
+
+            builder.RegisterAssemblyTypes(assemblies)
+             .AsClosedTypesOf(typeof(IEventHandler<>))
+             .InstancePerDependency();
+
+            return builder;
+        }
+    }
+}
